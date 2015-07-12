882877dae69e5d3ba1af0ec69db282760ec29ba0
okaoka <mnfrf593@yahoo.co.jp>
2015-07-13 00:09:07 +0900

    updated java-mode not to use tab indent

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -172,4 +172,8 @@
 (when (require 'flymake-php nil t)
   (add-hook 'php-mode-hook 'flymake-php-load))
 
-
+;;java-mode
+(add-hook 'java-mode-hook (lambda ()
+			    (setq c-basic-offset 4
+				  tab-width 4
+				  indent-tabs-mode nil)))
